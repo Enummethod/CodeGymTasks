4cb5b83fcac6bbd068a037d12e5e7d9b09d2506d
Mustafa Yaşar <80968031+Enummethod@users.noreply.github.com>
2021-11-23 13:35:40 +0300

    23.11.2021

---
+++ 1-Java Syntax/Level 0/2.I think being a programmer is cool/Solution.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task01.task0101;
+
+/* 
+I think being a programmer is cool
+
+*/
+
+// Mustafa YAŞAR
+
+public class Solution {
+    public static void main(String[] args) {
+    
+       System.out.println(" I think being a programmer is cool");
+    }
+}
---
+++ 1-Java Syntax/Level 0/3.Kind words for the teacher/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task01.task0102;
+
+/* 
+Kind words for the teacher
+
+*/
+
+// Mustafa YAŞAR
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Ellie is clever");
+        System.out.println("Ellie is clever");
+        System.out.println("Ellie is clever");
+        System.out.println("Ellie is clever");
+        System.out.println("Ellie is clever");
+    }
+}
